0bbfd238b2dc3ac42d5825ca8c4ccabbdd3dc6fb
Sarah-intake40 <58911690+Sarah-intake40@users.noreply.github.com>
2020-02-28 00:49:29 +0200

    no need

--- Post/Post/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import post , Category , Comment , Reply
-
-# Register your models here.
-admin.site.register(post)
-admin.site.register(Category)
-admin.site.register(Reply)
-admin.site.register(Comment)
